7343ed3374ae05b0e293eba5ce7bc4923b8a3e16
RyabekMk <rysbekmuktaraliuulu98@gmail.com>
2023-08-17 18:52:14 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        HashSet<Integer> set1 = new HashSet<>();
+        HashSet<Integer> set2 = new HashSet<>();
+        set1.add(1);
+        set1.add(2);
+        set1.add(45);
+        set2.add(34);
+        set2.add(45);
+        set1.add(3);
+        set2.add(0);
+        set2.add(1);
+        set2.add(2);
+
+        System.out.println(symmetricDifference(set1, set2));
+
+
+    }
+
+    public static Set<Integer> symmetricDifference(Set<Integer> set1, Set<Integer> set2) {
+
+        Set<Integer> setDifference = new HashSet<>();
+        for (Integer integer : set1) {
+            if (!set2.contains(integer)) {
+                setDifference.add(integer);
+            }
+        }
+        for (Integer integer : set2) {
+            if (!set1.contains(integer)) {
+                setDifference.add(integer);
+            }
+        }
+
+        return setDifference;
+    }
+}
